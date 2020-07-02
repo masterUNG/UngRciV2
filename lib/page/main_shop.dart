f1bbb707a72f9eb314fea2f2910299ab3e38182d
masterUNG <phrombutr@gmail.com>
2020-07-02 15:03:12 +0700

    update

--- lib/page/main_shop.dart
+++ lib/page/main_shop.dart
@@ -9,7 +9,30 @@ class _MainShopState extends State<MainShop> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Wellcome Shop'),),
+      drawer: showDrawer(),
+      appBar: AppBar(
+        title: Text('Wellcome Shop'),
+      ),
     );
   }
+
+  Drawer showDrawer() {
+    return Drawer(
+      child: Column(
+        children: <Widget>[
+          UserAccountsDrawerHeader(accountName: null, accountEmail: null),
+          menuMyOrder(),
+        ],
+      ),
+    );
+  }
+
+  ListTile menuMyOrder() => ListTile(
+        leading: Icon(Icons.looks_one),
+        title: Text('Show My Order'),
+        subtitle: Text('ดูรายการสั่งของ'),
+        onTap: () {
+          Navigator.pop(context);
+        },
+      );
 }
