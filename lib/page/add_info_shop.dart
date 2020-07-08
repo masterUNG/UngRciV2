289257a2c34d8a4b65a8c12fde2a3430c10b90ef
masterUNG <phrombutr@gmail.com>
2020-07-08 11:45:35 +0700

    update 11.45

--- lib/page/add_info_shop.dart
+++ lib/page/add_info_shop.dart
@@ -8,7 +8,15 @@ class AddInfoShop extends StatefulWidget {
 }
 
 class _AddInfoShopState extends State<AddInfoShop> {
-  String dateTimeString;
+  String dateTimeString, gender, educateString;
+  List<String> educates = [
+    'ต่ำกว่า ป.6',
+    'มัธยมต้น',
+    'มัธยมปลาย',
+    'ปริญาตรี',
+    'ปริญาโท',
+    'ปริญาเอก'
+  ];
 
   @override
   void initState() {
@@ -27,15 +35,114 @@ class _AddInfoShopState extends State<AddInfoShop> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: FloatingActionButton(onPressed: null),
       appBar: AppBar(
         title: Text('เพิ่มข้อมูล ร้านค้า'),
       ),
-      body: Column(
-        children: <Widget>[
-          MyStyle().showTextH2('วันเวลาที่ บันทึก'),
-          MyStyle().showTextH1(dateTimeString == null ? '' : dateTimeString)
-        ],
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            titleDate(),
+            showDate(),
+            addressForm(),
+            phoneForm(),
+            genderGroup(),
+            educateGroup(),
+          ],
+        ),
       ),
     );
   }
+
+  Container educateGroup() => Container(
+        child: DropdownButton<String>(
+          value: educateString,
+          items: educates
+              .map(
+                (e) => DropdownMenuItem(
+                  child: Text(e),
+                  value: e,
+                ),
+              )
+              .toList(),
+          hint: Text('Please Choose Education'),
+          onChanged: (value) {
+            setState(() {
+              educateString = value;
+            });
+          },
+        ),
+      );
+
+  Container genderGroup() => Container(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            maleRadio(),
+            femaleRadio(),
+          ],
+        ),
+      );
+
+  Row maleRadio() {
+    return Row(
+      children: <Widget>[
+        Radio(
+          value: 'Male',
+          groupValue: gender,
+          onChanged: (value) {
+            setState(() {
+              gender = value;
+            });
+          },
+        ),
+        Text('ชาย'),
+      ],
+    );
+  }
+
+  Row femaleRadio() {
+    return Row(
+      children: <Widget>[
+        Radio(
+          value: 'Female',
+          groupValue: gender,
+          onChanged: (value) {
+            setState(() {
+              gender = value;
+            });
+          },
+        ),
+        Text('หญิง'),
+      ],
+    );
+  }
+
+  Row showDate() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        MyStyle().showTextH1(dateTimeString == null ? '' : dateTimeString),
+      ],
+    );
+  }
+
+  Widget titleDate() => MyStyle().showTextH2('วันเวลาที่ บันทึก');
+
+  Widget addressForm() => Container(
+        margin: EdgeInsets.only(top: 16),
+        width: 250,
+        child: TextField(
+          decoration: MyStyle().myInputDecoration('ที่อยู่ :'),
+        ),
+      );
+
+  Widget phoneForm() => Container(
+        margin: EdgeInsets.only(top: 16),
+        width: 250,
+        child: TextField(
+          keyboardType: TextInputType.phone,
+          decoration: MyStyle().myInputDecoration('เบอร์โทรศัพย์ :'),
+        ),
+      );
 }
