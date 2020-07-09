72484489f2bac2605862aed6a0fb12d7791c5714
masterUNG <phrombutr@gmail.com>
2020-07-09 11:00:34 +0700

    update 11

---
+++ lib/page/add_product_shop.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:ungrci/utility/my_style.dart';
+
+class AddProductShop extends StatefulWidget {
+  @override
+  _AddProductShopState createState() => _AddProductShopState();
+}
+
+class _AddProductShopState extends State<AddProductShop> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Add Product'),
+      ),
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            imageGroup(),
+            nameForm(),
+            priceForm(),
+            detailForm(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget nameForm() => Container(
+        width: 250,
+        margin: EdgeInsets.only(top: 16),
+        child: TextField(
+          decoration: MyStyle().myInputDecoration('Name Product :'),
+        ),
+      );
+
+  Widget priceForm() => Container(
+        width: 250,
+        margin: EdgeInsets.only(top: 16),
+        child: TextField(
+          decoration: MyStyle().myInputDecoration('Price Product :'),
+        ),
+      );
+
+  Widget detailForm() => Container(
+        width: 250,
+        margin: EdgeInsets.only(top: 16),
+        child: TextField(
+          decoration: MyStyle().myInputDecoration('Detail Product :'),
+        ),
+      );
+
+  Widget imageGroup() => Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          IconButton(icon: Icon(Icons.add_a_photo), onPressed: null),
+          Container(
+            padding: EdgeInsets.all(16),
+            width: 200,
+            height: 200,
+            child: Image.asset('images/pic.png'),
+          ),
+          IconButton(icon: Icon(Icons.add_photo_alternate), onPressed: null)
+        ],
+      );
+}
--- lib/widget/show_my_product.dart
+++ lib/widget/show_my_product.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ungrci/page/add_product_shop.dart';
 
 class ShowMyProduct extends StatefulWidget {
   @override
@@ -8,8 +9,17 @@ class ShowMyProduct extends StatefulWidget {
 class _ShowMyProductState extends State<ShowMyProduct> {
   @override
   Widget build(BuildContext context) {
-    return Text(
-      'This is My Product'
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          MaterialPageRoute route = MaterialPageRoute(
+            builder: (context) => AddProductShop(),
+          );
+          Navigator.push(context, route).then((value) => null);
+        },
+        child: Icon(Icons.restaurant_menu),
+      ),
+      body: Text('This is My Product123'),
     );
   }
 }
