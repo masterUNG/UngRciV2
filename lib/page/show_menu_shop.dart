a60bf2c937b083315d08f5178c1859eb79914a8f
masterUNG <phrombutr@gmail.com>
2020-07-15 10:44:13 +0700

    อัพเดท

---
+++ lib/page/show_menu_shop.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:ungrci/models/user_model.dart';
+
+class ShowMenuShop extends StatefulWidget {
+  final UserModel userModel;
+  ShowMenuShop({Key key, this.userModel}) : super(key: key);
+
+  @override
+  _ShowMenuShopState createState() => _ShowMenuShopState();
+}
+
+class _ShowMenuShopState extends State<ShowMenuShop> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
